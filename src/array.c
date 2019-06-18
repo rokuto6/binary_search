b1eba79059e1f73a73f572ddabfc012be1707c19
Kokubun Yuta <ro.kyu@icloud.com>
2019-06-18 16:24:08 +0900

    array

--- src/array.c
+++ src/array.c
@@ -8,11 +8,16 @@ int A[100000];
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
+  lb = -1;
+  ub = n;
+  for(i = 0 ; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
-
+  while(ub-lb>1){
+    int p = (ub + lb) / 2;
+    if(A[p]<k) lb = p;
+    else ub = p;
+  }
+  printf("%d\n", ub);
   return 0;
 }
