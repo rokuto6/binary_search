0b441ce16ed8d8f045387770e5faa3d4af9bdf65
Kokubun Yuta <ro.kyu@icloud.com>
2019-06-18 16:53:54 +0900

    途中apple

--- src/apple.c
+++ src/apple.c
@@ -7,11 +7,19 @@ int A[100000];
 
 int main(){
   int i, lb, ub;
+  int sum = 0;
+  lb = 0;
   scanf("%d%d", &n, &k);
+  ub = 100000000/k;
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+    sum += A[i];
   }
-
-
+  while(ub-lb>1){
+    int m = (lb+ub)/2;
+    if((sum/m)<k) lb = m;
+    else ub = m;
+  }
+  printf("%d\n", ub);
   return 0;
 }
