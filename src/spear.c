191604b06f5f56221c546017fb89c3bec1671548
Kokubun Yuta <ro.kyu@icloud.com>
2019-06-19 12:06:27 +0900

    spear modified

--- src/spear.c
+++ src/spear.c
@@ -8,12 +8,14 @@ int A[100000];
 int main(){
   int i, lb, ub;
   lb = 0;
-  ub = 1000000001;
   int sum = 0;
+  int max = 0;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
+    if(max<A[i]) max = A[i];
   }
+  ub = max + 1;
   while(ub-lb>1){
     sum = 0;
     int m = (ub+lb)/2;
