a2c974448141fedd1bfc356e827c6eec9c36b3c0
Kokubun Yuta <ro.kyu@icloud.com>
2019-07-09 14:15:43 +0900

    complete

--- src/works.c
+++ src/works.c
@@ -3,15 +3,39 @@
 int n;
 int k;
 int A[100000];
-
+int i, j, lb, ub;
+int sum = 0;
+int p(int x){
+    int asum = 0;
+    int count = 1;
+    for(i = 0; i <= n; i++){
+        if(A[i] > x) return 0;
+        if(asum + A[i] <= x) asum += A[i];
+        else{
+            count++;
+            asum = A[i];
+        }
+    }
+    if(count <= k) return 1;
+    else return 0;
+}
 
 int main(){
-  int i, lb, ub;
-  scanf("%d%d", &n, &k);
-  for(i = 0; i < n; i++){
-    scanf("%d", &A[i]);
-  }
-
-
+    scanf("%d%d", &n, &k);
+    for(i = 0; i < n; i++){
+        scanf("%d", &A[i]);
+        sum += A[i];
+    }   
+    lb = (sum-1)/k;
+    ub = sum;
+    while(ub-lb>1){
+        int m = (lb+ub)/2;
+        if(p(m)){
+            ub = m;
+        }else{
+            lb = m;
+        }
+    }
+  printf("%d\n", ub);
   return 0;
 }
